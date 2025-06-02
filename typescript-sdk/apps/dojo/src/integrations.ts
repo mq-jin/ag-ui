2722cda7ef64b94202c52f72cabb5e88399e652a
Markus Ecker <markus.ecker@gmail.com>
2025-06-02 16:03:26 +0200

    add example

--- typescript-sdk/apps/dojo/src/integrations.ts
+++ typescript-sdk/apps/dojo/src/integrations.ts
@@ -1,5 +1,7 @@
 import { configureIntegration } from "./types/integration";
 import { StarterAgent } from "@ag-ui/starter";
+import { MastraClient } from "@mastra/client-js";
+
 export const integrations = [
   configureIntegration({
     id: "starter",
@@ -9,6 +11,18 @@ export const integrations = [
       return {
         agentic_chat: new StarterAgent(),
       };
+    },
+  }),
+
+  configureIntegration({
+    id: "mastra",
+    name: "Mastra",
+    features: ["agentic_chat"],
+    agents: async () => {
+      const mastra = new MastraClient({
+        baseUrl: "http://localhost:4111",
+      });
+      return "???" as any;
     },
   }),
 ];
